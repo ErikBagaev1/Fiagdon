af849fe0d15c4268c512075d1f095da8bd70e277
Erik Bagaev <BAGAEFF.ERIK@MAIL.RU>
2023-01-30 01:01:42 +0300

    добавление Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:fiagdon/Basik/basik_display.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+    apiKey: "AIzaSyDWhmxnklL73elt1x2lsbi6uG8LWPfiYJQ",
+    appId: "1:796201984509:android:662fb9ec35b2c0a2d0f994",
+    messagingSenderId: "796201984509",
+    projectId: "fiagdon-9b1f1",
+    storageBucket: "fiagdon-9b1f1.appspot.com",
+  ));
   runApp(const MyApp());
 }
 
